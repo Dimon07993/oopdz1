b5190db4f2a86c6032351aa2fa33618a17e602cf
qw <qw>
2024-04-12 09:59:11 +0300

    edit __add__ in class Product

--- utils/code.py
+++ utils/code.py
@@ -1,4 +1,11 @@
 import json
+from abc import ABC, abstractmethod
+
+
+class Qwerty(ABC):
+    @abstractmethod
+    def full_pra(self):
+        pass
 
 
 class Category():
@@ -59,7 +66,7 @@ class Category():
         return f'Название категории: {self.name}, кол-во продуктов: {len(self)} шт.'
 
 
-class Product():
+class Product(Qwerty):
     name: str
     description: str
     price: float
@@ -102,10 +109,15 @@ class Product():
         return f'Название продукта, {(self.price)} руб. Остаток: {self.count_in_stock} шт.'
 
     def __add__(self, other):
-        if not isinstance(other, type(self)):
+        if not isinstance(other, self.__class__):
+            raise TypeError('Операнды должны быть одного типа')
+        elif not isinstance(self, other.__class__):
             raise TypeError('Операнды должны быть одного типа')
         else:
             return self.price * self.count_in_stock + other.price * other.count_in_stock
+
+    def full_pra(self):
+        return self.price * self.count_in_stock
 
 
 class ProductSmartphone(Product):
@@ -113,6 +125,7 @@ class ProductSmartphone(Product):
     model: str
     memory: int
     color: str
+
     def __init__(self, name, description, price, count_in_stock, manufacturer, model, memory, color):
         super().__init__(name, description, price, count_in_stock)
         self.manufacturer = manufacturer
@@ -128,80 +141,87 @@ class LawnGrass(Product):
 
     def __init__(self, name, description, price, count_in_stock, side_producer, time_germination, color):
         super().__init__(name, description, price, count_in_stock)
+        self.full_price = None
         self.side_producer = side_producer
         self.time_germination = time_germination
         self.color = color
 
 
-with open('products.json', encoding='utf-8') as f:
-    data = json.load(f)
+if __name__ == '__main__':
+    with open('products.json', encoding='utf-8') as f:
+        data = json.load(f)
 
-category = Category(data[0]['name'], data[0]['description'], data[0]['products'])
+    category = Category(data[0]['name'], data[0]['description'], data[0]['products'])
 
-for category_data in data:
-    category.get_count_category(1)
-    for product_data in category_data['products']:
-        category.get_count_product(1)
+    for category_data in data:
+        category.get_count_category(1)
+        for product_data in category_data['products']:
+            category.get_count_product(1)
 
-print('----')
-category.display()
+    print('----')
+    category.display()
 
-# category.products = "laptop", "High-performance laptop", 1500.0, 10
-# print('++++')
-# print(category.products)
-# print('----')
+    # category.products = "laptop", "High-performance laptop", 1500.0, 10
+    # print('++++')
+    # print(category.products)
+    # print('----')
 
-s = category.get_print_price
-for i in s:
-    print(f'{i["name"]}, {(i["price"])} руб. Остаток: {i["quantity"]} шт.')
+    s = category.get_print_price
+    for i in s:
+        print(f'{i["name"]}, {(i["price"])} руб. Остаток: {i["quantity"]} шт.')
 
-new_product = Product.new_product('laptop', 'High-performance laptop', 1500.0, 999)
-new_product.price = -500
-print(new_product.price)
+    new_product = Product.new_product('laptop', 'High-performance laptop', 1500.0, 999)
+    new_product.price = -500
+    print(new_product.price)
 
-print(new_product.format_list)
+    print(new_product.format_list)
 
-print('----- __str__ Category')
+    print('----- __str__ Category')
 
-categ_1 = Category("Electronics", "Category for electronic devices",
-                   [{'name': 1, 'description': 2, 'price': 3, 'quantity': 4},
-                    {'name': 2, 'description': 3, 'price': 4, 'quantity': 5}])
-print(categ_1)  # 9
+    categ_1 = Category("Electronics", "Category for electronic devices",
+                       [{'name': 1, 'description': 2, 'price': 3, 'quantity': 4},
+                        {'name': 2, 'description': 3, 'price': 4, 'quantity': 5}])
+    print(categ_1)  # 9
 
-print('----- __str__ Product')
+    print('----- __str__ Product')
 
-prod_1 = Product("Laptop", "High-performance laptop", 1500.0, 10)
-print(prod_1)
+    prod_1 = Product("Laptop", "High-performance laptop", 1500.0, 10)
+    print(prod_1)
 
-print('----- __add__ Product сложение экземпляров')
+    print('----- __add__ Product сложение экземпляров')
 
-prod_2 = Product("Smartphone", "Flagship smartphone", 1000.0, 20)  # price * count = 20_000
-prod_3 = Product("Smartphone_§", "Smartphone", 2000.0, 40)  # price * count = 80_000
-sum = prod_2 + prod_3
-print(sum)
+    prod_2 = Product("Smartphone", "Flagship smartphone", 1000.0, 20)  # price * count = 20_000
+    prod_3 = Product("Smartphone_§", "Smartphone", 2000.0, 40)  # price * count = 80_000
+    sum = prod_2 + prod_3
+    print(sum)
 
+    print('----- __add__ ProductSmartphone')
 
-print('----- __add__ ProductSmartphone')
+    ps = ProductSmartphone("Samsung Galaxy C23 Ultra", "256GB, Серий цвет, 200MP камера", 180000.0,
+                           10, 200, "C23", 256, "Серий")
+    print(ps)
 
-ps = ProductSmartphone("Samsung Galaxy C23 Ultra", "256GB, Серий цвет, 200MP камера", 180000.0,
-                       10, 200, "C23", 256, "Серий")
-print(ps)
-
-# print('----- __add__ Нельзя складывать разные экземпляры разных классов TypeError' )
-#
-# ps = ProductSmartphone("Samsung Galaxy C23 Ultra", "256GB, Серий цвет, 200MP камера", 180000.0,
-#                        10, 200, "C23", 256, "Серий")
-# lg = LawnGrass('Green Lawn', 'Beautiful green lawn grass', 20, 100,
-#                'USA', '2 weeks', 'Green')
-#
-# print(lg+ps)
-
-print('----- Добавление только экземпляра или наследника класса' )
-new_product1 = Product.new_product('laptop', 'High-performance laptop', 1500.0, 777)
-print(new_product1)
-print(new_product1.__repr__())
+    print('----- __add__ Нельзя складывать разные экземпляры разных классов TypeError')
 
 
 
+    ps = ProductSmartphone("Samsung Galaxy C23 Ultra", "256GB, Серий цвет, 200MP камера", 180000.0,
+                            10, 200, "C23", 256, "Серий")
+    prod_4 = Product("Smartphone_§", "Smartphone", 2000.0, 40)
 
+    # print(prod_4 + ps)
+    print(ps + prod_4)
+    # lg = LawnGrass('Green Lawn', 'Beautiful green lawn grass', 20, 100,
+    #                'USA', '2 weeks', 'Green')
+    #
+    # print(ps+lg)
 
+    print('----- Добавление только экземпляра или наследника класса')
+    new_product1 = Product.new_product('laptop', 'High-performance laptop', 1500.0, 777)
+    print(new_product1)
+    print(new_product1.__repr__())
+
+    print('----- full_price class LagGrass')
+    lg1 = LawnGrass('Green Lawn', 'Beautiful green lawn grass', 20, 100,
+                    'USA', '2 weeks', 'Green')
+    print(lg1.full_pra())
